15d547f723ffa5b6ca5d6fa5448b20db834c5fbf
XynanXDB <penpowerslider@gmail.com>
2020-11-21 11:36:18 +0800

    Project init

---
+++ TogeJam/Assets/Scripts/Systems/SystemInit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SystemInit
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void Init()
+    {
+        Application.targetFrameRate = 60;
+
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnStartup()
+    {
+        
+    }
+}
