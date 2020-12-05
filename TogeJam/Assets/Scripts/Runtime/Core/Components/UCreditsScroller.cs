d04c6cf92692b58605bf473c1fa07572e6129551
XynanXDB <penpowerslider@gmail.com>
2020-12-05 15:20:26 +0800

    Added scrolling credits

---
+++ TogeJam/Assets/Scripts/Runtime/Core/Components/UCreditsScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Core
+{
+    public class UCreditsScroller : MonoBehaviour
+    {
+        [SerializeField] protected RectTransform Rect;
+        [SerializeField] protected Vector3 EndVector;
+        [SerializeField] protected float ScrollSpeed;
+
+        void OnEnable()
+        {
+            Rect.localPosition = new Vector3(0.0f, -Screen.height, 0.0f);
+        }
+
+        void Update()
+        {
+            Rect.localPosition = Vector3.Lerp(Rect.localPosition, EndVector, ScrollSpeed * Time.deltaTime);
+        }
+    }
+}
