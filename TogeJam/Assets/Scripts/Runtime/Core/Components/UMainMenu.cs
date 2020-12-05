bed65deea626d1d345879aacea378067508e3eb2
XynanXDB <penpowerslider@gmail.com>
2020-12-05 18:38:24 +0800

    Fix Menu to Credits camera inssue

--- TogeJam/Assets/Scripts/Runtime/Core/Components/UMainMenu.cs
+++ TogeJam/Assets/Scripts/Runtime/Core/Components/UMainMenu.cs
@@ -2,22 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 namespace Game.Core
 {
     public class UMainMenu : MonoBehaviour
     {
         [SerializeField] protected GameObject Credits;
+        [SerializeField] protected CinemachineVirtualCamera MainMenuVCam;
+
+        void OnEnable()
+        {
+            MainMenuVCam.Priority = 10;
+        }
+
+        void OnDisable()
+        {
+            MainMenuVCam.Priority = 0;
+        }
+
+        void Disable()
+        {
+            StartCoroutine(WaitandChangePriority());
+        }
+
+        IEnumerator WaitandChangePriority()
+        {
+            yield return new WaitForSeconds(1.0f);
+            gameObject.SetActive(false);
+        }
 
         public void OnClickPlay()
         {
-            gameObject.SetActive(false);
+            Disable();
+
             UPlayableDirector.PlayableDirector.PlayCinematic("Beat1_Start");
         }
 
         public void OnClickCredits()
         {
-            gameObject.SetActive(false);
+            Disable();
+
             UPlayableDirector.PlayableDirector.OnStop = () =>
                 {
                     Credits.SetActive(true);
